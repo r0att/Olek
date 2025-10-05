96d9a10c0f40afa701b8de607e65a6ccc7d81ad3
r0att <j.m.lowca@wp.pl>
2025-10-05 14:32:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function profile_oppopup(){
+    document.getElementById("profile_op").style.display="flex";
+}
 
+function profile_clpopup(){
+    document.getElementById("profile_op").style.display="none";
+}
