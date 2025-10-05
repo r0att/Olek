cac53098215e01ec8a8318d3b195f73297ebb067
PaVcixX <pawel.jamula08@gmail.com>
2025-10-05 16:33:10 +0200

    Update script_clicker.js

--- script_clicker.js
+++ script_clicker.js
@@ -1,1 +1,14 @@
+let wielkoscOlka = 0;
 
+function przytyj()
+{
+  wielkoscOlka ++;
+  document.getElementById("licznik").innerText = "Waga Olka: " + wielkoscOlka;
+        
+  const buttonClicker = document.getElementById("buttonClicker");
+
+  if (wielkoscOlka >= 100)
+  {
+    buttonClicker.classList.add("upgradeRare");
+  }
+}
